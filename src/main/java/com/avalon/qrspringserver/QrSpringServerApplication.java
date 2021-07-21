df77ce4d9d715fbca53462eae7b4b805df5c925a
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-22 01:31:58 +0300

    inital commit

---
+++ src/main/java/com/avalon/qrspringserver/QrSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.avalon.qrspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avalon/qrspringserver/QrSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalon.qrspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
