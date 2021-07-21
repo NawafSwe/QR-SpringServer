faf72f93ac78341bc96e1cf476b6b4249af6324e
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-22 02:20:05 +0300

    feat: adding item class

---
+++ src/main/java/model/Item.java
@@ -0,0 +1,26 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Item")
+public class Item {
+    private
+    @GeneratedValue
+    @Id
+    String id;
+    private String description;
+    private Float price;
+    // cannot set array of thing, it must be extracted to another class
+    // private String[] images;
+}
