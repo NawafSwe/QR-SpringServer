e8df4d772ea934d68b9890defdf5306274441bd3
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-22 06:59:32 +0300

    adding assembler util

---
+++ src/main/java/com/avalon/qrspringserver/utils/RestaurantAssembler.java
@@ -0,0 +1,14 @@
+package com.avalon.qrspringserver.utils;
+
+import com.avalon.qrspringserver.model.Restaurant;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestaurantAssembler implements RepresentationModelAssembler<Restaurant, EntityModel<Restaurant>> {
+    @Override
+    public EntityModel<Restaurant> toModel(Restaurant entity) {
+        return null;
+    }
+}
