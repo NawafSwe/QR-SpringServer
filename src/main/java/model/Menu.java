56d023ddff46051928302991e9eb859f03287816
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-22 03:03:02 +0300

    adding menu model

---
+++ src/main/java/model/Menu.java
@@ -0,0 +1,30 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Menu")
+public class Menu {
+    private @Id
+    @GeneratedValue
+    String id;
+
+    private String name;
+    // of type image maybe
+    private String logo;
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "PK_Category", nullable = false)
+    private List<Category> categories;
+
+    // images maybe
+}
