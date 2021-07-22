5f25b2d72da0709c25d7244b2a06360e0c7e643e
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-23 02:20:42 +0300

    proper handling for put req in restaurant

--- src/main/java/com/avalon/qrspringserver/controller/RestaurantController.java
+++ src/main/java/com/avalon/qrspringserver/controller/RestaurantController.java
@@ -4,7 +4,15 @@ import com.avalon.qrspringserver.model.Restaurant;
 import com.avalon.qrspringserver.repository.RestaurantRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import lombok.SneakyThrows;
+import org.apache.coyote.Response;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.xmlunit.validation.ValidationProblem;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -40,13 +48,23 @@ public class RestaurantController {
 
 
     @PutMapping(path = "/{id}")
-    Restaurant update(HttpServletRequest request, @PathVariable String id) throws IOException {
+    ResponseEntity<?> update(HttpServletRequest request, @PathVariable String id) {
         ObjectMapper mapper = new ObjectMapper();
         Restaurant findRestaurant = repository.findById(id)
                 .orElseThrow();
-        Restaurant updateRestaurant = mapper.readerForUpdating(findRestaurant).readValue(request.getReader());
-        findRestaurant.setUpdatedAt(new Date());
-        return repository.saveAndFlush(updateRestaurant);
+        try {
+            Restaurant updateRestaurant = mapper.readerForUpdating(findRestaurant).readValue(request.getReader());
+            findRestaurant.setUpdatedAt(new Date());
+            return ResponseEntity
+                    .ok(repository.saveAndFlush(updateRestaurant));
+        } catch (IOException error) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                    .body(Problem.create()
+                            .withTitle("Internal Server Error")
+                            .withDetail("Error while mapping, internal server Error")
+                    );
+        }
     }
 
     @DeleteMapping(path = "/{id}")
