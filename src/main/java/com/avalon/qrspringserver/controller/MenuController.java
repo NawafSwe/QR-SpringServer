b7cc6f8a867e45d04ca8a37e43f864aa5674773b
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-24 00:24:55 +0300

    removing used imports

--- src/main/java/com/avalon/qrspringserver/controller/MenuController.java
+++ src/main/java/com/avalon/qrspringserver/controller/MenuController.java
@@ -7,10 +7,6 @@ import com.avalon.qrspringserver.model.Restaurant;
 import com.avalon.qrspringserver.repository.MenuRepository;
 import com.avalon.qrspringserver.repository.RestaurantRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.coyote.Response;
-import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.mediatype.problem.Problem;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class MenuController {
