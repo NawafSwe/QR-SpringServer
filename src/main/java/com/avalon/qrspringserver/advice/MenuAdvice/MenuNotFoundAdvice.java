66494c892371385169cdb25713dee0a77b933c0c
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-24 00:16:42 +0300

    complete Menu not found advice

--- src/main/java/com/avalon/qrspringserver/advice/MenuAdvice/MenuNotFoundAdvice.java
+++ src/main/java/com/avalon/qrspringserver/advice/MenuAdvice/MenuNotFoundAdvice.java
@@ -1,7 +1,23 @@
 package com.avalon.qrspringserver.advice.MenuAdvice;
 
+import com.avalon.qrspringserver.error.MenuErrors.MenuNotFound;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
 
 @ControllerAdvice
 public class MenuNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(MenuNotFound.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Menu Not Found")
+    public HashMap<String, String> message(MenuNotFound ex) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("message", ex.getMessage());
+        map.put("status", "404");
+        return map;
+    }
 }
