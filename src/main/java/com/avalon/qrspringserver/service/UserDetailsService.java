a0300340f7dc872652bb8e5a309ac7e904a202c1
NawafSwe <nawaf.jr9@hotmail.com>
2021-08-01 00:26:46 +0300

    adding UserDetailsService

---
+++ src/main/java/com/avalon/qrspringserver/service/UserDetailsService.java
@@ -0,0 +1,4 @@
+package com.avalon.qrspringserver.service;
+
+public class UserDetailsService {
+}
--- src/main/java/com/avalon/qrspringserver/utils/SecurityConfig.java
+++ src/main/java/com/avalon/qrspringserver/utils/SecurityConfig.java
@@ -3,6 +3,11 @@ package com.avalon.qrspringserver.utils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,6 +15,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
+
+
+        // the strategy we use bcrypt
+//    String idForEncode = "bcrypt";
+//    Map encoders = new HashMap<>();
+//        encoders.put(idForEncode, new BCryptPasswordEncoder());
+//        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+//        encoders.put("scrypt", new SCryptPasswordEncoder());
+//    PasswordEncoder passwordEncoder =
+//            new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 
 }
