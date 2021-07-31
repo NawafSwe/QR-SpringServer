9293d10487f3d68061224c8e386ff00ce8f9969c
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-31 23:35:25 +0300

    creating user model

---
+++ src/main/java/com/avalon/qrspringserver/model/User.java
@@ -0,0 +1,42 @@
+package com.avalon.qrspringserver.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "User")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Id
+    String id;
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    private Date joinedAt;
+
+    @OneToOne
+    @JoinColumn(name = "rest_PK", nullable = false)
+    private Restaurant restaurant;
+}
