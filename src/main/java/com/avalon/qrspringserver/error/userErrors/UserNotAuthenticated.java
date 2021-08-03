599d6dfda65ace29ef44efb80e41f44b94a25087
NawafSwe <nawaf.jr9@hotmail.com>
2021-08-03 18:09:22 +0300

    feat:UserNotAuthenticated expection

---
+++ src/main/java/com/avalon/qrspringserver/error/userErrors/UserNotAuthenticated.java
@@ -0,0 +1,16 @@
+package com.avalon.qrspringserver.error.userErrors;
+
+public class UserNotAuthenticated extends RuntimeException {
+    private final String message;
+
+    public UserNotAuthenticated(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
+}
